590fde6aa7c2ef7a71105c2a26b54eb705b2424d
oosesmarin <oosesmarin@educacion.navarra.es>
2025-12-17 10:27:31 +0100

    Constructor añadido

--- Examen_Oscar/src/Examen/Libro.java
+++ Examen_Oscar/src/Examen/Libro.java
@@ -5,4 +5,12 @@ public class Libro {
     private String autor;
     private String isbn;
     private boolean prestado;
+    
+	public Libro(String titulo, String autor, String isbn, boolean prestado) {
+		this.titulo = titulo;
+		this.autor = autor;
+		this.isbn = isbn;
+		this.prestado = prestado;
+	}
+    
 }
